a6c656718c583524451d535d03b8b8dd446600f2
Paul126512 <121176953+Paul126512@users.noreply.github.com>
2025-06-12 22:08:51 +0200

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET;
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
