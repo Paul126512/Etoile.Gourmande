300be752af555e5f34b80f61dde965cf0f5028a9
Paul126512 <121176953+Paul126512@users.noreply.github.com>
2025-06-15 14:26:48 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
 // pages/_app.js
-import React from "react";
-import { Analytics } from "@vercel/analytics/react"; // Pour Vercel Analytics (statistiques de visiteurs)
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      {/* Composant de collecte des statistiques de visiteurs */}
       <Analytics />
-
-      {/* Composant de collecte des métriques de performance */}
-      <SpeedInsights />
     </>
   );
 }
